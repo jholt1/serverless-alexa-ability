e359804a39213d1e036f9fbaf64d0d98ac15e584
Jonathan Holt <jonathan.a.holt@gmail.com>
2017-07-20 15:16:14 +0100

    splice

--- src/ability.js
+++ src/ability.js
@@ -114,7 +114,7 @@ export class Ability {
       let __intents__ = attributes.__intents__;
 
       this.ev.handler = intent;
-      __intents__.splice(0, __intents__.length -1);
+      __intents__.splice(__intents__.length -1, 1);
       this.session({__intents__});
       this.sent = true;
       this[last.type](last.message).converse();
